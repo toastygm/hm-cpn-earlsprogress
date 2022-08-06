0f99f836f9c4954bcd2dc57ad69f81330a399b94
Tom Rodriguez <tmrodrig@gmail.com>
2022-08-06 16:33:31 -0700

    Change welcome journal

--- scripts/init.js
+++ scripts/init.js
@@ -7,7 +7,7 @@
    * Set to the following to disable:
    *   const welcomeJournal = '';
    */
-  const welcomeJournal = '_Overview';
+  const welcomeJournal = "Earl's Progress";
   /**
    * additionalJournals will automatically be imported.
    * Set to the following to disable:
